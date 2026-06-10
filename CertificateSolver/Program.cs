cfec0db06c03f25b7cb23c7c99b31cc24f9ed160
kawaion <egorfedorycev@mail.ru>
2026-06-10 12:39:12 +0400

    Add run

--- CertificateSolver/Program.cs
+++ CertificateSolver/Program.cs
@@ -10,7 +10,31 @@ namespace CertificateSolver
     {
         public static void Main(string[] args)
         {
+            var builder = WebApplication.CreateBuilder(args);
 
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddSingleton<IRequestStatusTransition, RequestStatusTransition>();
+            builder.Services.AddSingleton<IIdempotencyStore, IdempotencyStore>();
+            builder.Services.AddSingleton<IIdempotencyKeyGenerator, IdempotencyKeyGenerator>();
+            builder.Services.AddSingleton<IRequestRepository, RequestRepository>();
+            builder.Services.AddScoped<IRequestService, RequestService>();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+
+            app.Run();
         }
     }
 }
